vickyleu/kotlin

Bundle IJ coroutines fork

prepare/compiler/build.gradle.kts
@@ -150,7 +150,7 @@ dependencies {
     api(kotlinStdlib("jdk8"))
     api(project(":kotlin-script-runtime"))
     api(commonDependency("org.jetbrains.kotlin:kotlin-reflect")) { isTransitive = false }
-    api(libs.kotlinx.coroutines.core)
+    embedded("com.intellij.platform:kotlinx-coroutines-core-jvm:1.8.0-intellij-13") { isTransitive = false }
 
     proguardLibraries(project(":kotlin-annotations-jvm"))
 
@@ -168,8 +168,6 @@ dependencies {
         libraries(kotlinStdlib(classifier = "distJsJar"))
         libraries(kotlinStdlib(classifier = "distJsKlib"))
     }
-
-    librariesStripVersion(libs.kotlinx.coroutines.core) { isTransitive = false }
 
     distLibraryProjects.forEach {
         libraries(project(it)) { isTransitive = false }

Tencent-TDS/KuiklyBase-kotlin

prepare/compiler/build.gradle.kts
@@ -152,7 +152,7 @@ dependencies {
     api(kotlinStdlib("jdk8"))
     api(project(":kotlin-script-runtime"))
     api(commonDependency("org.jetbrains.kotlin:kotlin-reflect")) { isTransitive = false }
-    api(libs.kotlinx.coroutines.core)
+    fatJarContents("com.intellij.platform:kotlinx-coroutines-core-jvm:1.8.0-intellij-13") { isTransitive = false }
 
     proguardLibraries(project(":kotlin-annotations-jvm"))
 
@@ -170,8 +170,6 @@ dependencies {
         libraries(kotlinStdlib(classifier = "distJsJar"))
         libraries(kotlinStdlib(classifier = "distJsKlib"))
     }
-
-    librariesStripVersion(libs.kotlinx.coroutines.core) { isTransitive = false }
 
     distLibraryProjects.forEach {
         libraries(project(it)) { isTransitive = false }