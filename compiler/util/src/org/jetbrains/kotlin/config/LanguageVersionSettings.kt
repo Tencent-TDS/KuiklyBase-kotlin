shirrumon/kotlin

KT-54363 Allow using reified types for catch parameters

compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -308,6 +308,7 @@ enum class LanguageFeature(
     MangleCallsToJavaMethodsWithValueClasses(KOTLIN_2_0, kind = OTHER), // KT-55945
     ForbidInferringTypeVariablesIntoEmptyIntersection(KOTLIN_2_0, kind = BUG_FIX), // KT-51221
     ProhibitDefaultArgumentsInExpectActualizedByFakeOverride(KOTLIN_2_0, kind = BUG_FIX), // KT-62036
+    AllowReifiedTypeInCatchClause(KOTLIN_2_0, kind = BUG_FIX), // KT-54363
 
     // 2.1
 

Tencent-TDS/KuiklyBase-kotlin

Co-authored-by: Alexander Udalov <alexander.udalov@jetbrains.com>

compiler/util/src/org/jetbrains/kotlin/config/LanguageVersionSettings.kt
@@ -406,6 +406,7 @@ enum class LanguageFeature(
     UnstableSmartcastOnDelegatedProperties(KOTLIN_2_3, kind = BUG_FIX), // KTLC-273
     ForbidAnnotationsWithUseSiteTargetOnExpressions(KOTLIN_2_3, kind = BUG_FIX), // KT-75242
     ProhibitNullableTypeThroughTypealias(KOTLIN_2_3, kind = BUG_FIX), // KTLC-279
+    AllowReifiedTypeInCatchClause(KOTLIN_2_3, kind = OTHER), // KT-54363
 
     // End of 2.* language features --------------------------------------------------
 